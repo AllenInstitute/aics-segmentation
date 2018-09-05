1f03decf01be9ce178d42c1c0ee8ecfe9b84d2c4
Jianxu Chen <jianxuc@alleninstitute.org>
2018-09-05 13:15:53 -0700

    setup gradle

---
+++ aicssegmentation/__init__.py
@@ -0,0 +1,7 @@
+from .version import MODULE_VERSION
+
+
+def get_module_version():
+    return MODULE_VERSION
+
+
---
+++ aicssegmentation/tests/dummy_test.py
@@ -0,0 +1,7 @@
+from time import sleep
+from datetime import timedelta
+
+
+def test_dummy():
+    assert(True)
+
