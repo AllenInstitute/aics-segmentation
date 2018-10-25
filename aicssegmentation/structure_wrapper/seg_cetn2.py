6af0c4498c3eaea1dbf85d5d569569346fe4561c
Jianxu Chen <jxchen.ustc@gmail.com>
2018-10-25 10:17:13 -0700

    fix a type, should be CETN2, not CENT2, renaming seg_cent2.py to  seg_cetn2.py

---
+++ aicssegmentation/structure_wrapper/seg_cetn2.py
@@ -0,0 +1,65 @@
+import numpy as np
+import os
+from skimage.morphology import remove_small_objects, watershed, dilation, ball
+from ..pre_processing_utils import intensity_normalization, image_smoothing_gaussian_slice_by_slice
+from ..core.seg_dot import dot_3d
+from skimage.feature import peak_local_max
+from scipy.ndimage import distance_transform_edt
+from skimage.measure import label
+
+
+def CETN2_HiPSC_Pipeline(struct_img,rescale_ratio):
+    ##########################################################################
+    # PARAMETERS:
+    #   note that these parameters are supposed to be fixed for the structure
+    #   and work well accross different datasets
+
+    intensity_norm_param = [8000]
+    gaussian_smoothing_sigma = 1
+    gaussian_smoothing_truncate_range = 3.0
+    dot_3d_sigma = 1
+    dot_3d_cutoff = 0.04
+    minArea = 3
+    ##########################################################################
+
+    ###################
+    # PRE_PROCESSING
+    ###################
+    # intenisty normalization (min/max)
+    struct_img = intensity_normalization(struct_img, scaling_param=intensity_norm_param)
+    
+    # rescale if needed
+    if rescale_ratio>0:
+        struct_img = processing.resize(struct_img, [1, rescale_ratio, rescale_ratio], method="cubic")
+        struct_img = (struct_img - struct_img.min() + 1e-8)/(struct_img.max() - struct_img.min() + 1e-8)
+        gaussian_smoothing_truncate_range = gaussian_smoothing_truncate_range * rescale_ratio
+
+    # smoothing with gaussian filter
+    structure_img_smooth = image_smoothing_gaussian_slice_by_slice(struct_img, sigma=gaussian_smoothing_sigma, truncate_range=gaussian_smoothing_truncate_range)
+
+    ###################
+    # core algorithm
+    ###################
+
+    # step 1: LOG 3d 
+    response = dot_3d(structure_img_smooth, log_sigma=dot_3d_sigma)
+    bw = response > dot_3d_cutoff
+    bw = remove_small_objects(bw>0, min_size=minArea, connectivity=1, in_place=False)
+
+    # step 2: 'local_maxi + watershed' for cell cutting
+    local_maxi = peak_local_max(struct_img,labels=label(bw), min_distance=2, indices=False)
+    distance = distance_transform_edt(bw)
+    im_watershed = watershed(-distance, label(dilation(local_maxi, selem=ball(1))), mask=bw, watershed_line=True)
+
+    ###################
+    # POST-PROCESSING
+    ###################
+    seg = remove_small_objects(im_watershed, min_size=minArea, connectivity=1, in_place=False)
+
+    # output
+    seg = seg>0
+    seg = seg.astype(np.uint8)
+    seg[seg>0]=255
+
+    return seg
+
