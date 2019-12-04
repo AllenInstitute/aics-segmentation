87787be6ebb3f0e924098f037830a53aae3c0b18
Jianxu Chen <jianxuc@alleninstitute.org>
2019-12-04 14:46:33 -0800

    update for ASCB demo
    Former-commit-id: 14b8d07934189951d4cc7a0bf0f8bad2bafedf38

---
+++ aicssegmentation/cli/objectify.py
@@ -0,0 +1,48 @@
+import os 
+import numpy as np
+from scipy.ndimage.measurements import label
+
+def simple_objectifier(bw, se=None, return_dataframe=False):
+
+    if se is None:
+        obj_label, obj_num = label(bw>0)
+    else:
+        obj_label, obj_num = label(bw>0, structure=se)
+
+    if return_dataframe:
+        import pandas as pd
+        obj_df = pd.DataFrame(np.arange(1,obj_num+1,1), columns=['object_index'])
+        return obj_label, obj_df
+    else:
+        return obj_label
+
+def masked_objectifier(bw, mask_label):
+
+    assert mask_label.max()>0
+
+    if mask_label.max()==1:
+        mask_label, num_label = label(mask_label>0)
+        label_list = np.arange(1, num_label+1, 1)
+    else:
+        label_list = np.unique(mask_label[mask_label>0])
+
+    counter_offset = 0
+    obj_label = np.zeros_like(mask_label)
+    multi_index_list = []
+    for ii, val in enumerate(label_list):
+        single_mask = mask_label==val
+        valid_bw = bw.copy()
+        valid_bw[single_mask==0]=0
+        valid_label, valid_label_num = label(valid_bw>0)
+        for jj, valid_index in range(valid_label_num):
+            index_plus = valid_index + 1
+            this_obj_index = index_plus + counter_offset
+            this_obj = valid_label==index_plus
+            obj_label[this_obj] = this_obj_index
+            multi_index_list.append([val,this_obj_index, this_obj.sum()])
+        counter_offset = counter_offset + valid_label_num
+
+    obj_df = pd.DataFrame(multi_index_list)
+    obj_df.columns=['mask_id', 'obj_id', 'vol']
+    
+    return obj_df.set_index(['mask_id', 'obj_id'])
