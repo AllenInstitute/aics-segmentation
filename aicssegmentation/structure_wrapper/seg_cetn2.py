48b8836d2dbe6687cd2b466ed813d7ebb17f4431
Jianxu Chen <jianxuc@alleninstitute.org>
2020-08-19 11:01:52 -0700

    update CETN2 algorithm

--- aicssegmentation/structure_wrapper/seg_cetn2.py
+++ aicssegmentation/structure_wrapper/seg_cetn2.py
@@ -16,7 +16,8 @@ def Workflow_cetn2(struct_img,rescale_ratio, output_type, output_path, fn, outpu
     #   note that these parameters are supposed to be fixed for the structure
     #   and work well accross different datasets
 
-    intensity_norm_param = [8000]
+    intensity_norm_param_seg = [12, 160, 300, 2000]
+    intensity_norm_param_peak = [5000]
     gaussian_smoothing_sigma = 1
     gaussian_smoothing_truncate_range = 3.0
     dot_3d_sigma = 1
@@ -31,21 +32,24 @@ def Workflow_cetn2(struct_img,rescale_ratio, output_type, output_path, fn, outpu
     # PRE_PROCESSING
     ###################
     # intenisty normalization (min/max)
-    struct_img = intensity_normalization(struct_img, scaling_param=intensity_norm_param)
+    struct_img_for_seg = intensity_normalization(struct_img.copy(), scaling_param=intensity_norm_param_seg)
+    struct_img_for_peak = intensity_normalization(struct_img.copy(), scaling_param=intensity_norm_param_peak)
 
-    out_img_list.append(struct_img.copy())
+    out_img_list.append(struct_img_for_seg.copy())
     out_name_list.append('im_norm')
     
     # rescale if needed
     if rescale_ratio>0:
-        struct_img = resize(struct_img, [1, rescale_ratio, rescale_ratio], method="cubic")
-        struct_img = (struct_img - struct_img.min() + 1e-8)/(struct_img.max() - struct_img.min() + 1e-8)
+        struct_img_for_seg = resize(struct_img_for_seg, [1, rescale_ratio, rescale_ratio], method="cubic")
+        struct_img_for_seg = (struct_img_for_seg - struct_img_for_seg.min() + 1e-8)/(struct_img_for_seg.max() - struct_img_for_seg.min() + 1e-8)
+        struct_img_for_peak = resize(struct_img_for_peak, [1, rescale_ratio, rescale_ratio], method="cubic")
+        struct_img_for_peak = (struct_img_for_peak - struct_img_for_peak.min() + 1e-8)/(struct_img_for_peak.max() - struct_img_for_peak.min() + 1e-8)
         gaussian_smoothing_truncate_range = gaussian_smoothing_truncate_range * rescale_ratio
 
     # smoothing with gaussian filter
-    structure_img_smooth = image_smoothing_gaussian_slice_by_slice(struct_img, sigma=gaussian_smoothing_sigma, truncate_range=gaussian_smoothing_truncate_range)
+    structure_img_smooth_for_seg = image_smoothing_gaussian_slice_by_slice(struct_img_for_seg, sigma=gaussian_smoothing_sigma, truncate_range=gaussian_smoothing_truncate_range)
 
-    out_img_list.append(structure_img_smooth.copy())
+    out_img_list.append(structure_img_smooth_for_seg.copy())
     out_name_list.append('im_smooth')
 
     ###################
@@ -53,7 +57,7 @@ def Workflow_cetn2(struct_img,rescale_ratio, output_type, output_path, fn, outpu
     ###################
 
     # step 1: LOG 3d 
-    response = dot_3d(structure_img_smooth, log_sigma=dot_3d_sigma)
+    response = dot_3d(structure_img_smooth_for_seg, log_sigma=dot_3d_sigma)
     bw = response > dot_3d_cutoff
     bw = remove_small_objects(bw>0, min_size=minArea, connectivity=1, in_place=False)
 
@@ -61,7 +65,7 @@ def Workflow_cetn2(struct_img,rescale_ratio, output_type, output_path, fn, outpu
     out_name_list.append('interm_mask')
 
     # step 2: 'local_maxi + watershed' for cell cutting
-    local_maxi = peak_local_max(struct_img,labels=label(bw), min_distance=2, indices=False)
+    local_maxi = peak_local_max(struct_img_for_peak, labels=label(bw), min_distance=2, indices=False)
 
     out_img_list.append(local_maxi.copy())
     out_name_list.append('interm_local_max')
